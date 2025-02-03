cd4dff180c28aec8c59346b9bb9a8f771870a8c8
Grace John <gracekjohn123@gmail.com>
2025-02-03 19:22:37 +0530

    admincreate done

--- src/main/java/grace/onlinecart/mycart/controller/AdminController.java
+++ src/main/java/grace/onlinecart/mycart/controller/AdminController.java
@@ -9,9 +9,11 @@ import grace.onlinecart.mycart.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.ui.Model;
+import grace.onlinecart.mycart.model.Customer;
 
 import jakarta.servlet.http.HttpSession;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 
@@ -22,12 +24,24 @@ public class AdminController {
     @GetMapping("")
     public String home(Model model ,HttpSession session) {
         model.addAttribute("session",session);
-        //model.addAttribute("data",service.read());
+        model.addAttribute("data",service.read());
         return "admin/home";
     }
     
     @GetMapping("/create")
     public String create() {
+        return "/admin/create";
+    }
+    
+    @PostMapping("/create")
+    public String createPost(@ModelAttribute Customer customer,Model model) {
+        Customer c=service.create(customer);
+        if(c==null){
+            model.addAttribute("msg","Not able to create!Try later!");
+        }
+        else{
+            model.addAttribute("msg",c.getName()+" Registered");
+        }
         return "/admin/create";
     }
 
